f5f9552bc78155f96cafee7315360268b92ad561
diaszano <luucas.dsantos@gmail.com>
2023-12-19 22:04:47 -0300

    docs: Documentado o código

--- internal/scoreboard/scoreboard.go
+++ internal/scoreboard/scoreboard.go
@@ -1,3 +1,7 @@
+// Package scoreboard provides functionality to manage and persist a collection of scores.
+//
+// The package includes methods to save and load scores from a JSON file, as well as a Score struct
+// representing an individual score entry.
 package scoreboard
 
 import (
@@ -7,22 +11,28 @@ import (
 	"time"
 )
 
+// Score represents an individual score entry with a name, points, and creation timestamp.
 type Score struct {
 	Name    string    `json:"name"`
 	Points  uint8     `json:"points"`
 	Created time.Time `json:"created"`
 }
 
+// scoreboardCollection represents a collection of scores.
 type scoreboardCollection struct {
 	Scores []Score `json:"scores"`
 }
 
 const fileName = "points.json"
 
+// Save appends a new score to the existing scoreboard and saves it to the file.
+//
+// It loads the existing scores, appends the new score, and writes the updated scoreboard
+// to the file. If any error occurs during the process, it returns the existing scores and the error.
 func Save(score Score) (scores []Score, err error) {
 	scores, err = Load()
 	if err != nil {
-		return scores, fmt.Errorf("")
+		return scores, fmt.Errorf("failed to load scores: %v", err)
 	}
 	scores = append(scores, score)
 	ad := scoreboardCollection{Scores: scores}
@@ -30,72 +40,48 @@ func Save(score Score) (scores []Score, err error) {
 	return scores, nil
 }
 
+// write writes the scoreboardCollection to the specified file in JSON format.
+//
+// It creates or truncates the file, marshals the scoreboardCollection to JSON, and writes
+// the data to the file. If any error occurs during this process, it returns an error.
 func write(scoreboard scoreboardCollection) error {
 	file, err := os.Create(fileName)
 	if err != nil {
-		return fmt.Errorf("failed to create the scoreboardCollection %s: %v", fileName, err)
+		return fmt.Errorf("failed to create file %s: %v", fileName, err)
 	}
 	defer file.Close()
 
 	data, err := json.MarshalIndent(scoreboard, "", "\t")
 	if err != nil {
-		return fmt.Errorf(": %v", err)
+		return fmt.Errorf("failed to marshal JSON: %v", err)
 	}
 
 	_, err = file.Write(data)
 	if err != nil {
-		return fmt.Errorf("failed to write the scoreboardCollection %s: %v", fileName, err)
+		return fmt.Errorf("failed to write file %s: %v", fileName, err)
 	}
 	return nil
 }
 
-// Load function reads the scoreboardCollection data from a file and returns the list of scores.
+// Load reads the scoreboardCollection data from a file and returns the list of scores.
 //
 // It internally uses the read function to read the data from the file specified by fileName.
-// If the read operation is successful, the function returns the list of scores and a nil error.
 // If any error occurs during the file reading or JSON parsing, an error message is returned,
 // and the scores slice is empty.
-//
-// Example:
-//
-//	scores, err := Load()
-//	if err != nil {
-//	    log.Fatal(err)
-//	}
-//	// Now 'scores' contains the list of scores from the scoreboardCollection.
-//
-// Returns:
-//   - scores: A slice of Score struct representing the scores loaded from the scoreboardCollection.
-//   - err: An error indicating the success or failure of the load operation.
 func Load() (scores []Score, err error) {
 	scoreboard, err := read()
 	if err != nil {
-		return scores, fmt.Errorf("failed to load the scoreboardCollection %s: %v", fileName, err)
+		return scores, fmt.Errorf("failed to load scores: %v", err)
 	}
 
 	scores = scoreboard.Scores
 	return scores, nil
 }
 
-// read function reads a JSON file and parses its content into a scoreboardCollection.
-// The file specified by the fileName parameter should contain valid JSON data
-// representing the scoreboardCollection structure.
+// read reads a JSON file and parses its content into a scoreboardCollection.
 //
-// If the file is successfully read and parsed, the function returns the populated
-// scoreboardCollection and a nil error. If any error occurs during file reading or JSON parsing,
-// an error message is returned along with an empty scoreboardCollection.
-//
-// Example:
-//
-//	table, err := read()
-//	if err != nil {
-//	    log.Fatal(err)
-//	}
-//	// Now 'table' contains the parsed data.
-//
-// Returns:
-//   - table: A scoreboardCollection struct containing the parsed data from the file.
-//   - err: An error indicating the success or failure of the read operation.
+// If any error occurs during file reading or JSON parsing, an error message is returned along
+// with an empty scoreboardCollection.
 func read() (scoreboard scoreboardCollection, err error) {
 	file, err := os.ReadFile(fileName)
 	if err != nil {
@@ -115,7 +101,7 @@ func read() (scoreboard scoreboardCollection, err error) {
 
 	err = json.Unmarshal(file, &scoreboard)
 	if err != nil {
-		return scoreboard, fmt.Errorf("fails to parse JSON: %v", err)
+		return scoreboard, fmt.Errorf("failed to parse JSON: %v", err)
 	}
 
 	return scoreboard, nil
